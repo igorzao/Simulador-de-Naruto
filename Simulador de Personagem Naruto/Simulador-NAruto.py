215fc1631526ba8918e7a76563e9812d89a8d6a4
Igor Yukio <108741584+igorzao@users.noreply.github.com>
2023-02-08 02:04:17 -0300

    Add files via upload

---
+++ Simulador de Personagem Naruto/Simulador-NAruto.py
@@ -0,0 +1,27 @@
+from random import random, choice, uniform
+
+print("No Mundo Ninja voce teria o elemento:")
+elemento = ["Fogo", "Agua", "Raio", "Vento", "Terra"]
+print(choice(elemento))
+
+print("-------" * 10)
+
+print("E seria do cla:")
+cla = ["Uchiha", "Uzumaki", "Senju", "Hyuga", "Sarutobi", "Comum", "Kaguya"]
+print(choice(cla))
+
+
+print("-------" * 10)
+
+print("E moraria na vila da(o):")
+Vila = ["Folha", "Areia", "Pedra", "Nevoa", "Nuvem", "Chuva", "Cachoeiras"]
+print(choice(Vila))
+
+print("-------" * 10)
+
+print("E seria um:")
+classe = ["Civil", "Gennin", "Chunnin", "Jounnin", "Ambu", "Kage", "Renegado"]
+print(choice(classe))
+
+
+
